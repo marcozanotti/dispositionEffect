e58cfda57ebc5d88b3946f540a9f89ef6287f01f
Marco Zanotti <zanottimarco17@gmail.com>
2021-02-15 16:38:43 +0100

    new function to check gainloss

--- R/utils-checks.R
+++ R/utils-checks.R
@@ -4,6 +4,7 @@
 #'
 #' @description Functions to perform checks on arguments.
 #'
+#' @param df Data frame
 #' @param df_name Chr
 #' @param df_names Chr
 #' @param target_names Chr
@@ -19,6 +20,46 @@
 #'
 #' @keywords internal
 NULL
+
+
+#' @describeIn checks Check consistency of gains and losses "values" results.
+check_gainloss <- function(df) {
+
+	rg <- df$RG_value < 0
+	rl <- df$RL_value > 0
+	pg <- df$PG_value < 0
+	pl <- df$PL_value > 0
+
+	msg <- vector("list", 4)
+
+	if (sum(rg) > 0) {
+		msg[[1]] <- paste("RG value < 0 for asset(s)",
+											paste(df$asset[rg], collapse = ", "), "\n")
+	}
+
+	if (sum(rl) > 0) {
+		msg[[2]] <- paste("RL value > 0 for asset(s)",
+											paste(df$asset[rl], collapse = ", "), "\n")
+	}
+
+	if (sum(pg) > 0) {
+		msg[[3]] <- paste("PG value < 0 for asset(s)",
+											paste(df$asset[pg], collapse = ", "), "\n")
+	}
+
+	if (sum(pl) > 0) {
+		msg[[4]] <- paste("PL value > 0 for asset(s)",
+											paste(df$asset[pl], collapse = ", "), "\n")
+	}
+
+	res_msg <- paste(purrr::compact(msg), collapse = "")
+	if (res_msg == "") {
+		res_msg <- NULL
+	}
+
+	return(res_msg)
+
+}
 
 
 #' @describeIn checks Check consistency of data frame variables' names.

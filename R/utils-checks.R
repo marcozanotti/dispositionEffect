cabd8e9e4a5719f8aecc614a1608865f1996b029
Marco Zanotti <zanottimarco17@gmail.com>
2021-03-01 13:41:34 +0100

    added generic check_values

--- R/utils-checks.R
+++ R/utils-checks.R
@@ -7,11 +7,54 @@
 #' @param df Data frame
 #' @param input_values Input to check against target.
 #' @param target_values Target to check for in input.
+#' @param no_exception Logical. If TRUE different values of inputs from targets
+#'   are not allowed to exist.
+#' @param weak_target Logical. If TRUE inputs do not need to contain all the
+#'    targets.
 #'
-#' @return A character string containing an error message or NULL.
+#' @return A vector or list containing the wrong values.
 #'
 #' @keywords internal
 NULL
+
+
+#' @describeIn checks Check consistency of values against targets and vice versa.
+check_values <- function(input_values, target_values, no_exception = FALSE, weak_target = FALSE) {
+
+	tar_idx <- target_values %in% input_values
+	inp_idx <- input_values %!in% target_values
+
+	if (weak_target) {
+		tar_test <- any(tar_idx)
+	} else {
+		tar_test <- all(tar_idx)
+	}
+
+	if (no_exception) {
+
+		if (tar_test & !any(inp_idx)) {
+			res <- list("target" = NULL, "input" = NULL)
+		} else if (tar_test & any(inp_idx)) {
+			res <- list("target" = NULL, "input" = input_values[inp_idx])
+		} else if (!tar_test & any(inp_idx)) {
+			res <- list("target" = target_values[!tar_idx], "input" = input_values[inp_idx])
+		} else {
+			res <- list("target" = target_values[!tar_idx], "input" = NULL)
+		}
+
+	} else {
+
+		if (tar_test) {
+			res <- NULL
+		} else {
+			res <- target_values[!tar_idx]
+		}
+
+	}
+
+	return(res)
+
+}
 
 
 #' @describeIn checks Check consistency of gains and losses "values" results.
@@ -43,23 +86,6 @@ check_gainloss <- function(df) {
 	res_msg <- paste(msg, collapse = "")
 	if (res_msg == "") {
 		res_msg <- NULL
-	}
-
-	return(res_msg)
-
-}
-
-
-#' @describeIn checks Check consistency of market prices "values" results.
-check_market_prices <- function(input_values, target_values) {
-
-	idx <- target_values %in% input_values
-
-	if (all(idx)) {
-		res_msg <- NULL
-	} else {
-		res_msg <- paste("No market prices available for asset(s)", paste(target_values[!idx], collapse = ", "), "\n")
-
 	}
 
 	return(res_msg)
